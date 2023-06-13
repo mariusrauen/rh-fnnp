2c104fcd688b9c599fd9f682c3904b305923c59c
Jonas Goßen <jonas.gossen@gmail.com>
2023-06-13 10:06:36 +0200

    fixed some typos in the main.py comments.

--- 01_create_system_expansion/src/main.py
+++ 01_create_system_expansion/src/main.py
@@ -2,14 +2,14 @@ import calc_automated_extension_layer as calc
 
 def main(input_file_path, output_file_path):
     """Get System expansion and matrix table mass"""
-    #1.) load all data from the reactions the excel sheet
+    #1.) Load all data from the reactions the excel sheet.
     matrix_table = calc.load_prepare_data(input_file_path)
   
-    #2) check if the carbon content is balanced
+    #2) Check if the carbon content is balanced.
     calc.carbon_content_check(matrix_table)
 
-    #3) the mass of all raw materials and products is calculted based
-    # based on the stoichometric coefficents and normalized of 1kg product (mainflow)
+    #3) The mass of all raw materials and products is calculated based
+    #   on the stoichometric coefficents and normalized of 1kg product (mainflow).
     matrix_mass = calc.calculate_mass(matrix_table)
 
     #4) A yield of 0.95% is considered for the reactions.
@@ -18,15 +18,15 @@ def main(input_file_path, output_file_path):
     #5) The energy demand is calculated according to Gendorf.
     matrix_demand = calc.calculate_energy_demand_Gendorf(matrix_mass, matrix_table)
 
-    #6) the new mass based flows are saved in an excel file. 
+    #6) The new mass based flows are saved in an excel file. 
     matrix_table_mass = calc.create_preprocessing_mass_table(matrix_mass, matrix_demand)
 
-    #7) A matrix is created from the raw material and paroduct mass flows based on 
-    # the matrix table design.
+    #7) A matrix is created from the raw material and product mass flows based on 
+    #   the matrix table design.
     matrixA = calc.matrix_creation(matrix_table_mass ,'process_id','raw_material_id','mass', 'raw materials', 'process')
     
     # The final systemexpansion table is created including process_meta_data, summary A, 
-    # Summery B, and summary F
+    # summary B, and summary F.
     calc.create_system_expansion(matrixA, output_file_path)
     print("Done!!!")
 
